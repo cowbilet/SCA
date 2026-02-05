e9351aaa46bce570a5177072dfb851c7bf5aa1d3
Cowbilet <sebgtaylor@gmail.com>
2026-02-05 14:09:44 +1100

    pulled the drizzle db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,8 @@ import { defineConfig } from 'drizzle-kit'
 config({ path: ['.env.local', '.env'] })
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
-})
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
