4e8f41b26c4eee37ab093c1aa5fac2d2dff48ef4
Cowbilet <sebgtaylor@gmail.com>
2026-03-06 15:22:00 +1100

    created the log DB functions

---
+++ src/db/logs.server.ts
@@ -0,0 +1,18 @@
+import { logs } from "./schema.server";
+import { db } from "./index.server";
+import type { Award} from "@/types/awards";
+import type { Challenge } from "@/types/challenges";
+import { and, eq } from "drizzle-orm";
+export async function dbGetStudentAwardLogs(studentId: string, award: Award, challenge: Challenge) {
+    return await db.select().from(logs).where((log) => and(eq( log.studentId, studentId), eq(log.award, award), eq(log.challenge, challenge)))
+}
+export async function dbCreateLogEntry(studentId: string, award: Award, challenge: Challenge, date: string, description: string, mentorId: string) {
+    return await db.insert(logs).values({
+        studentId,
+        award,
+        challenge,
+        date,
+        description,
+        mentorId,
+    }).returning()
+}
--- src/types/schemas/log.ts
+++ src/types/schemas/log.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-
+import type { Award } from "@/types/awards";
+import type { Challenge } from "@/types/challenges";
 export const CreateLogEntrySchema = z.object({
     date: z.coerce.date<string>().refine((date) => {
         const today = new Date()
@@ -9,3 +10,19 @@ export const CreateLogEntrySchema = z.object({
     }),
     description: z.string(),
 })
+export const LogEntrySchema = z.object({
+    logId: z.uuid(),
+    studentId: z.uuid(),
+    award: z.string().refine((value): value is Award => ['bronze', 'silver', 'gold'].includes(value), {
+        message: 'Invalid award tier',
+    }),
+    challenge: z.string().refine((value): value is Challenge => ['challenge1', 'challenge2', 'challenge3'].includes(value), {
+        message: 'Invalid challenge',
+    }),
+    date: z.coerce.date<string>(),
+    description: z.string(),
+    mentorId: z.uuid(),
+    approved: z.boolean(),
+    evidence: z.string(),
+})
+export type LogEntry = z.infer<typeof LogEntrySchema>
